8c83bd42651516c359f0fa28ac38b5382acfe86b
Rehman Ali <rehmanali.9442289@gmail.com>
2021-01-01 16:53:19 +0500

    update

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ client = discord.Client()
 dbQueue = Queue()
 f = open('config.json', 'r')
 config = json.load(f)
+set1 = config.get("SET1")
+set2 = config.get("SET2")
+set3 = config.get("SET3")
 
 
 @client.event
@@ -15,17 +18,67 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+async def valid(text):
+    set1Found = False
+    set2Found = False
+    set3Found = False
+    for w in set1:
+        if w.lower() in text.lower():
+            set1Found = True
+            print('Set 1 passed!')
+    for w in set2:
+        if w.lower() in text.lower():
+            set2Found = True
+            print('Set 2 passed!')
+    for w in set3:
+        if w.lower() in text.lower():
+            set3Found = True
+            print('Set 3 passed!')
+    if '$' in text:
+        set3Found = True
+        print('$ passed!')
+
+    def createWords(text):
+        sublists = [s for s in text.split('\n')]
+        words = []
+        for l in sublists:
+            finalList = l.split(' ')
+            words.extend([w for w in finalList])
+        words = list(filter(None, words))
+        string = ' '.join(words)
+        words = ''.join(
+            ch for ch in string if ch.isalnum() or ch == ' ')
+        return [w for w in words.split(' ')]
+
+    words = createWords(text)
+    for w in words:
+        if len(w) <= 4 and len(w) >= 1:
+            if w.isupper():
+                set3Found = True
+    if set1Found and set2Found and set3Found:
+        print('All tests passed!')
+        return True
+    return False
+
+messages = 0
+
+
 @client.event
 async def on_message(message):
+    global messages
+    messages += 1
+    print(f'Messages : {messages}')
     guildsList = config.get("SERVERS").keys()
     guild = message.guild.name
     channel = message.channel.name
     if guild in guildsList:
+        print('Valid Server')
         channels = config.get("SERVERS").get(guild)
         if channel in channels:
-            for word in config.get("KEYWORDS"):
-                if word.lower() in message.content.lower():
-                    await sendToTarget(message)
+            print('Valid channel')
+            if await valid(message.content):
+                print('Sending to target!')
+                await sendToTarget(message)
 
 
 async def sendToTarget(message):
